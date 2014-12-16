e25c18ed54bfa3bbb253fbdf2e34d7f0bc91eafd
yuntang <1282823258@qq.com>
2014-12-16 21:40:50 +0800

    add index.php

---
+++ src/php/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>站内搜索</title>
+    <meta http-equiv=content-type content="text/html; charset=utf-8">
+</head>
+<body>
+    <div style="width:600px;height:300px;position:absolute;margin-left:360px;margin-top:100">
+    <form action="search_result.php" method="post">
+        <font size=100, color="green">北邮人毕业生版搜索</font><br>
+        <input type="text" name="key_words", style="width:400px;height:40px;font-size:60px;">
+        <input type="submit" value="搜 索 一 下", style="width:100px;height:40px">
+    </form>
+    </div>
+</body>
+</html>
